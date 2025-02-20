321cbe1f8da4deca30b9b0ff5ff9e5d885ecc3f9
jyanga11 <jyanga12@gmail.com>
2025-02-20 10:53:18 -0600

    init commit

---
+++ App/backend/app.py
@@ -0,0 +1,135 @@
+from flask import Flask, request, jsonify, make_response
+import json
+import os
+from flask_cors import CORS
+import numpy as np
+from music21 import stream, chord
+import tensorflow as tf
+from tensorflow.keras.models import load_model
+from sklearn.preprocessing import LabelEncoder
+
+app = Flask(__name__)
+CORS(app)
+
+
+# Load the trained LSTM model
+model = load_model("checkpoint-49.h5")
+
+# Load chord encoder
+encoder = LabelEncoder()
+encoder.classes_ = np.load("encoder_classes2.npy", allow_pickle=True)
+
+# Load notes values corresponding to each chord
+with open("../chords_data.json", "r") as f:
+    chords_notes = [list(c.values())[0] for c in json.load(f)]
+
+
+sequence_length = 4
+
+def sample_with_temperature_and_multiplier(predictions, temperature, history, repetitiveness):
+    """
+    Applies temperature sampling and multiplier
+    
+    - `predictions`: Raw logits from model output.
+    - `temperature`: Controls randomness (higher = more diverse).
+    - `history`: List of recent chords.
+    - `repetitiveness`: Multiplier applied to probabilities of recent chords (0.0-1.0).
+    """
+    predictions = np.asarray(predictions).astype("float64")
+
+    # Apply temperature scaling
+    predictions = np.log(predictions + 1e-8) / temperature  
+    exp_preds = np.exp(predictions)
+    probabilities = (exp_preds / np.sum(exp_preds))[0]
+
+    # Apply multiplier to recently played chords
+    for recent_chord in history:
+        rc = int(recent_chord.item())
+        probabilities[rc] *= repetitiveness
+
+    # Renormalize probabilities to sum to 1
+    probabilities /= np.sum(probabilities)
+
+    return np.random.choice(len(probabilities), p=probabilities)
+
+
+def generate_chord_sequence(seed_sequence, num_chords=4, temperature=1.0, window_size=4, repetitiveness=2.0):
+    """
+    Generate a chord sequence using temperature sampling for repetition.
+    
+    - `seed_sequence`: Initial chord sequence (numerical encoding).
+    - `num_chords`: Number of chords to generate.
+    - `temperature`: Randomness factor.
+    - `window_size`: How many past chords to consider for repetitiveness.
+    - `repetitiveness`: Weight to increase probability of recently played chords.
+    """
+    generated_chords = seed_sequence[:]
+    for _ in range(num_chords - len(seed_sequence)):
+        if len(seed_sequence) == 0:
+            random_seed = np.random.choice(len(encoder.classes_)-1)
+            input_seq = np.array(random_seed).reshape(1,-1)            # Start with a random chord
+        elif sequence_length <= len(seed_sequence):
+            input_seq = np.array(generated_chords[-sequence_length:]).reshape(1, -1)  # Get the last n for the starting sequence
+        else:
+            input_seq = np.array(generated_chords[:]).reshape(1, -1)   # Use the whole seed sequence
+
+        prediction = model.predict(input_seq, verbose=0)
+
+        history = generated_chords[-window_size:] if window_size > 0 else []
+
+        next_chord = sample_with_temperature_and_multiplier(prediction, temperature, history, repetitiveness)
+        generated_chords = np.append(generated_chords, next_chord)
+    
+    return generated_chords.astype(int)
+
+@app.route("/", methods=["OPTIONS", "GET"])
+def index():
+    return jsonify({"all_chords" : encoder.classes_.tolist()})
+
+@app.route("/generate", methods=["POST"])
+def generate():
+    data = request.json
+    num_chords = data.get("length", 4)
+    temperature = data.get("temperature", 1.0)
+    seed = data.get("selected_chords", [])
+    seed = [chord['value'] for chord in seed]
+    seed = encoder.transform(seed)
+    repetitiveness = data.get("repetitiveness", 2.0)
+    if repetitiveness == 0.0:
+        repetitiveness *= 0.01
+    elif repetitiveness == 1.0:
+        repetitiveness *= 5
+    elif repetitiveness == 2.0:
+        repetitiveness *= 20
+    elif repetitiveness == 3.0:
+        repetitiveness *= 100
+    elif repetitiveness == 4.0:
+        repetitiveness *= 10_000
+    generated_chords = generate_chord_sequence(seed, num_chords, temperature, repetitiveness=repetitiveness)
+
+    chords = encoder.inverse_transform(generated_chords)
+
+    notes = [chords_notes[c] for c in generated_chords]
+    notes = [chord.Chord(c) for c in notes]
+    
+    s = stream.Stream()
+    for c in notes:
+        c.quarterLength = 4  # Set duration (4 beats)
+        s.append(c)
+
+    # Save as MIDI file
+    midi_file_name = "static/chord_progression.mid"
+    os.makedirs("static", exist_ok=True)  # Ensure static folder exists
+    s.write("midi", fp=midi_file_name)
+    if os.path.exists(midi_file_name):
+        print("File created")
+    else:
+        print("File creation failed.")
+
+    return jsonify({
+        "chord_progression" : chords.tolist(),
+        "midi_url": "/static/chord_progression.mid"
+        })
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5000)
---
+++ App/frontend/src/App.js
@@ -0,0 +1,255 @@
+import { useEffect, useState } from "react"
+import Select from "react-select"
+import makeAnimated from 'react-select/animated';
+import { motion } from "framer-motion";
+
+
+function App() {
+  let [chordProgression, setChordProgression] = useState([]);
+  let [allChords, setAllChords] = useState([]);
+  let [selectedChords, setSelectedChords] = useState([]);
+  let [midiURL, setMidiURL] = useState(null);
+  let [length, setLength] = useState(4);
+  let [temperature, setTemperature] = useState(1.0);
+  let [repetitiveness, setRepetitiveness] = useState(2.0);
+  let [windowSize, setWindowSize] = useState(4);
+
+  const animatedComponents = makeAnimated();
+
+  // Gets a list of chords for the user to select
+  const getAllChords = async () => {
+    const response = await fetch("http://127.0.0.1:5000/", {
+      method : "GET",
+      headers: {"Content-Type" : "application/json" },
+    });
+
+    const data = await response.json();
+    let temp = []
+    data.all_chords.values().forEach(element => {
+      temp.push({"label": element, "value": element})
+    });
+    setAllChords(temp);
+  }
+
+  // Requests a chord progression from the backend
+  const generateChords = async () => {
+    if (selectedChords.length > length) {
+      alert("The given chord sequence is longer than the requested chord progression!");
+      return;
+    }
+
+    const response = await fetch("http://127.0.0.1:5000/generate", {
+      method : "POST",
+      headers : {"Content-Type" : "application/json" },
+      body : JSON.stringify({ 'length': length,
+                              'temperature': temperature,
+                              'repetitiveness': repetitiveness,
+                              'window_size': windowSize,
+                              'selected_chords': selectedChords
+                            }),
+    });
+
+    const data = await response.json();
+    setChordProgression(data.chord_progression);
+    setMidiURL(data.midi_url);
+  }
+
+  const downloadMIDI = () => {
+    if (midiURL) {
+      midiURL = `http://127.0.0.1:5000${midiURL}`;
+      const a = document.createElement('a');
+      a.href = midiURL;
+      a.download = "chord_progression.mid";
+      document.body.appendChild(a);
+      a.click();
+      document.body.removeChild(a);
+    }
+  }
+
+  // Ran on load
+  useEffect(() => {
+    getAllChords();    // Populates the select list
+  },[])
+
+  // Function to dynamically render div holders for chord names
+  // Based on the requested length of the chord progression
+  const renderBoxes = () => {
+    return Array.from({ length }).map((_, index) => (
+      <motion.div
+        key={index}
+        style={{
+          display:'flex',
+          flexDirection:'column',
+          justifyContent:'center',
+          alignItems:'center',
+          margin:'1em',
+          padding:'0.5em',
+          height:'7em',
+          width: '10em',
+          backgroundColor: '#8400ff',
+          borderRadius: '15px 50px',
+          fontSize: '1.1em',
+          fontWeight: 'bold',
+          color: 'white',
+          boxShadow: '0 4px 8px rgba(0, 0, 0, 0.2)',
+          transition: 'transform 0.2s ease, box-shadow 0.2s ease',
+        }}
+        whileHover={{ scale: 1.05, boxShadow: '0 6px 12px rgba(0, 0, 0, 0.3)' }}
+      >
+        {chordProgression[index] || '   \n   \n'}
+      </motion.div>
+    ));
+  };
+
+  // Custom styling for the select list
+  const customStyles = {
+    control: (base) => ({
+      ...base,
+      backgroundColor: "#8400ff",
+      "&:hover": { border: "1px solid #ff00f7"},
+    }),
+    option: (base, { isFocused, isSelected }) => ({
+      ...base,
+      backgroundColor: isSelected ? "#8400ff" : isFocused ? "#8400ff" : "#460385",
+      color: isSelected ? "#ff00f7" : "white",
+      fontWeight: 'bold',
+    }),
+  };
+  
+  return (
+    
+    <motion.div
+    className="mx-auto"
+    style={{
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: '100vh',
+      backgroundColor: '#460385',
+      color: 'white',
+    }}
+    initial={{ opacity: 0, scale: 0.9 }}
+    animate={{ opacity: 1, scale: 1 }}
+    transition={{ duration: 0.5 }}
+  >
+
+      <motion.div   // Chords
+        className="grid grid-cols-4 gap-4 grid-flow-row auto-rows-fr"
+        style={{
+          display: 'grid',
+          height:'50em',
+          gridTemplateColumns:'repeat(4, 1fr)',
+          padding:'2.5em',
+          textAlign: 'center',
+          margin: 'auto',
+          marginTop:'1em',
+          marginBottom: '0.2em',
+          justifyContent:'center',
+          alignContent:'center',
+        }}
+      >
+        {renderBoxes()}
+      </motion.div>
+
+      <div   /*Select list*/   >
+        <div style={{margin:'auto', marginBottom:'2em', width:'40em', alignContent:'center'}}>
+          <Select
+            styles={customStyles}
+            options={allChords}
+            value={selectedChords}
+            onChange={setSelectedChords}
+            placeholder="Select a chord sequence to begin with..."
+            isMulti
+            components={animatedComponents}
+          />
+        </div>
+
+        <div  // Settings Sliders
+          className="mt-8 space-y-6 w-full max-w-md"
+          style={{display:'flex', margin: '0.8em', marginBottom:'2em', justifyContent: 'center'}}
+        >
+          {[
+            { label: "Length", value: length, setter: setLength, min: 1, max: 8, step: 1 },
+            { label: "Temperature", value: temperature, setter: setTemperature, min: 0.1, max: 2.0, step: 0.1 },
+            { label: "Repetitiveness", value: repetitiveness, setter: setRepetitiveness, min: 0, max: 4, step: 1 },
+          ].map(({ label, value, setter, min, max, step }) => (
+            <div key={label} style={{marginLeft:'3em', marginRight:'3em'}}>
+              <label
+                className="text-white font-medium mb-2 flex items-center justify-between"
+                style={{fontWeight:'bold'}}
+              >
+                  {label}: {value}
+              </label>
+              <input
+                type="range"
+                min={min}
+                max={max}
+                step={step}
+                value={value}
+                onChange={(e) => setter(Number(e.target.value))}
+                className="w-full accent-teal-500 cursor-pointer"
+                style={{
+                  width: '100%',
+                  accentColor: '#ff00f7',
+                  cursor: 'pointer',
+                }}
+              />
+            </div>
+          ))}
+        </div>
+
+        <div 
+          className="mt-8 w-full max-w-md flex flex-col items-center space-y-4"
+          style={{margin:'auto', textAlign:'center', display:'flex', justifyContent: 'center'}}
+        >
+          <motion.button // Generate button
+            onClick={generateChords}
+            className="w-full"
+            style={{
+              backgroundColor: '#9200a8',
+              color: 'white',
+              fontSize: '1.2em',
+              fontWeight: 'bold',
+              padding: '1em',
+              marginRight: '2em',
+              marginBottom:'0.5em',
+              borderRadius: '15px',
+              boxShadow: '0 4px 12px rgba(0, 0, 0, 0.2)',
+              transition: 'transform 0.2s ease',
+            }}
+            whileHover={{ scale: 1.05 }}
+            whileTap={{ scale: 0.95 }}
+          >
+            Generate
+          </motion.button>
+
+
+          <motion.button  // Download button
+            onClick={downloadMIDI}
+            className="w-full"
+            style={{
+              backgroundColor: '#6A4C93',
+              color: 'white',
+              fontSize: '1.2em',
+              fontWeight: 'bold',
+              padding: '1em',
+              marginLeft: '2em',
+              marginBottom:'0.5em',
+              borderRadius: '15px',
+              boxShadow: '0 4px 12px rgba(0, 0, 0, 0.2)',
+              transition: 'transform 0.2s ease',
+            }}
+            whileHover={{ scale: 1.05 }}
+            whileTap={{ scale: 0.95 }}
+          >
+            Download
+          </motion.button>
+
+        </div>
+      </div>
+    </motion.div>
+  );
+}
+
+export default App;
